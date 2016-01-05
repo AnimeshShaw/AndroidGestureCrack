44a244d190aecbb63994fb3426a89bcb5785cae4
AnimeshShaw <psychocoder@outlook.com>
2016-01-05 08:10:11 +0530

    changed license header

--- src/net/letshackit/androidgesturecrack/tool/AGCGui.java
+++ src/net/letshackit/androidgesturecrack/tool/AGCGui.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Psycho_Coder.
+ * Copyright 2015-16 Animesh Shaw ( a.k.a. Psycho_Coder)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
